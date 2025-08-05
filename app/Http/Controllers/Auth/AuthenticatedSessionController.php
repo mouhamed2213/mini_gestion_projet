ec5a3ce58088e8e0a7733b54857034be290aca54
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-08-05 23:32:32 +0000

    feat: set up auth with sancutm

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse; // <-- Assurez-vous que cette ligne est bien en haut du fichier
+
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+//        $token = $user->createToken(config('app.name'))->accessToken;
+
+        return response()->json([
+            'token' => $token,
+        ]);
+    }
+
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
