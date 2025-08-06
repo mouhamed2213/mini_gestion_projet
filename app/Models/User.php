f958d9ab0981fb8bf422fa0dbb7e8dde8f055905
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-08-06 01:05:39 +0000

    create: set up project controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens; // <-- 1. AJOUTEZ CETTE LIGNE
@@ -35,6 +36,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * user  has many project
+     */
+
+    function projects () : HasMany {
+        return $this->HasMany(Projects::class);
+    }
+
 
     /**
      * Get the attributes that should be cast.
---
+++ app/Models/projects.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class projects extends Model
+{
+    // relation project belong to user
+    function owner() : belongsTo  {
+        return $this -> belongsTo (User::class, 'user_id');
+    }
+}
